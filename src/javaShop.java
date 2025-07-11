ef292b99df1b53877239b94cc11f11bd25f3f033
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-07-11 18:43:31 +0200

    creazione repo

---
+++ src/javaShop.java
@@ -0,0 +1,39 @@
+
+public class javaShop {
+    
+    public int codice;
+    public String nome;
+    public String descrizione;
+    public double prezzo;
+    public double iva;
+
+    public javaShop(){
+        
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+// Esercizio di oggi Java Shop
+// repo : java-oop-shop
+// Nel progetto java-oop-shop, creare la classe Prodotto che gestisce i prodotti dello shop.
+// Un prodotto è caratterizzato da:
+// - codice (numero intero)
+// - nome
+// - descrizione
+// - prezzo
+// - iva
+// Usate opportunamente costruttori, attributi ed eventuali altri metodi di “utilità” per fare in modo che:
+// - alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+// - il prodotto esponga un metodo per avere il prezzo base
+// - il prodotto esponga un metodo per avere il prezzo comprensivo di iva
+// - il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome
+// Aggiungete una classe Main con metodo main nella quale testate tutte le funzionalità della classe Prodotto.
