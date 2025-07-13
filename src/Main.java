ccdb3061e6d41c371d3d16fbb0bfdd9425976cba
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-07-13 12:17:03 +0200

    incapsulamento

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         
-        javaShop prodotto1 = new javaShop("tastiera");
-        prodotto1.prezzo = 150;
-        prodotto1.iva = 22;
+        javaShop prodotto1 = new javaShop("Tastiera", 150, 22);
 
-        prodotto1.nomeEsteso();
-        prodotto1.prezzoBase();
-        prodotto1.prezzoFinale();
- 
+            prodotto1.nomeEsteso();
+            prodotto1.prezzoBase();
+            prodotto1.prezzoFinale();
+
+        javaShop prodotto2 = new javaShop("Mouse", 80, 22);
+
+            prodotto2.nomeEsteso();
+            prodotto2.prezzoBase();
+            prodotto2.prezzoFinale();
+
+        javaShop prodotto3 = new javaShop("Scheda Video", 2200, 22);
+            
+            prodotto3.nomeEsteso();
+            prodotto3.prezzoBase();
+            prodotto3.prezzoFinale();
     }
 }
--- src/javaShop.java
+++ src/javaShop.java
@@ -1,15 +1,27 @@
 
 public class javaShop {
     
-    public int codice;
-    public String nome;
-    public String descrizione;
-    public double prezzo;
-    public double iva;
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private double prezzo;
+    private double iva;
 
-    public javaShop(String nome){
+    public javaShop(String nome, double prezzo, double iva){
         this.nome = nome;
         this.codice = (int)(Math.random()*5000);  //random.nextInt(bound:1000);
+        if(prezzo < 0){
+            System.out.println("Il prezzo non è valido");
+            return;
+        }else{
+            this.prezzo = prezzo;
+        }
+        if(iva < 0){
+            System.out.println("iva non valida");
+            return;
+        }else{
+            this.iva = iva;
+        }
     }
 
     public double prezzoBase(){
@@ -26,10 +38,47 @@ public class javaShop {
 
     public String nomeEsteso(){
         String nomeEsteso = codice + "-" + nome;
-        System.out.println("Il prodotto selezionato è " + nomeEsteso);
+        System.out.println("- Il prodotto selezionato è " + nomeEsteso);
         return nomeEsteso;
     }
 
+    public int getCodice() {
+        return codice;
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setPrezzo(double prezzo) {
+        if(prezzo < 0){
+            System.out.println("Il prezzo non è valido");
+        }else{
+            this.prezzo = prezzo;
+        }
+        
+    }
+
+    public void setIva(double iva) {
+        if(iva < 0){
+            System.out.println("iva non valida");
+        }else{
+            this.iva = iva;
+        }
+    }
+
+    
 
 }
